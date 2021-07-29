6f681a9ae74f168d8bc364fcc111cdf55c41f858
yeminaike <79099063+yeminaike@users.noreply.github.com>
2021-07-29 21:21:38 +0100

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,30 @@
+// document.getElementById("count-el").innerText=5
+
+// change the count-el in the HTML to reflect the new count
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+let count = 0
+
+function increment() {
+	count+=1
+countEl.textContent = count
+}
+
+function save() {
+	let countStr = count + " - "
+	saveEl.textContent += countStr
+	countEl.textcontent = 0
+	count = 0
+}
+
+
+
+// Create a function, save(), which logs out the count when it is called
+
+
+
+
+
